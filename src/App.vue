ffdf5968c84494b9118c707e17a033ebb9dab2f1
Gregor Laan <gregor@github.ee>
2018-03-24 15:41:32 +0200

    Replace default component with Home component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <main-header></main-header>
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -22,8 +21,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
