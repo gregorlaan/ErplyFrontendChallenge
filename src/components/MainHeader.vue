477c2ce62f64f271b64d797fa5cd821c0f0e4492
Gregor Laan <gregor@github.ee>
2018-03-24 18:10:28 +0200

    Insert Bootstrap based menu

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>Header</div>
+
+  <b-navbar toggleable="md" type="dark" variant="primary">
+
+    <b-container>
+
+      <b-navbar-brand href="#">E-shop SPA</b-navbar-brand>
+
+      <b-navbar-toggle target="nav_collapse" class="ml-auto"></b-navbar-toggle>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="cart">Cart</b-nav-item>
+        </b-navbar-nav>
+
+      </b-collapse>
+
+    </b-container>
+
+  </b-navbar>
+
 </template>
 
 <script>
