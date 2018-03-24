982b944ae83b6abb07dff71100854eb794a35758
Gregor Laan <gregor@github.ee>
2018-03-24 18:36:49 +0200

    Make a request for products

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,6 +4,29 @@
 
 <script>
 export default {
+  data () {
+    return {
+      products: {}
+    }
+  },
+  mounted: function () {
+    this.getData()
+  },
+  methods: {
+    getData: function () {
+      this.$http({
+        url: 'https://erply-challenge.herokuapp.com/list?AUTH=fae7b9f6-6363-45a1-a9c9-3def2dae206d',
+        method: 'GET'
+      }).then(function (response) {
+        // success callback
+        var data = response.body
+        this.products = data
+        console.log(data)
+      }, function (response) {
+        // error callback
+      })
+    }
+  }
 }
 </script>
 
