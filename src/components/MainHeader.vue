ae896b141dee01ef443dc736a403b62b910564c7
Gregor Laan <gregor@github.ee>
2018-03-25 21:36:00 +0300

    Make cart link path absolute

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -14,7 +14,7 @@
 
         <b-navbar-nav class="ml-auto">
           <b-nav-item to="/">Home</b-nav-item>
-          <b-nav-item to="cart">Cart</b-nav-item>
+          <b-nav-item to="/cart">Cart</b-nav-item>
         </b-navbar-nav>
 
       </b-collapse>
