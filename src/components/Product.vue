4a1fb6d8faede6a14122c6c201727691ef5dba3c
Gregor Laan <gregor@github.ee>
2018-03-25 19:50:27 +0300

    Get current product data

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,12 +1,45 @@
 <template>
-  <div>product {{ productId }}</div>
+  <div>
+
+    <b-container>
+      <b-row>
+        {{ currentProduct[0].name }}
+      </b-row>
+    </b-container>
+
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      productId: this.$route.params.productId
+      productsList: [],
+      productId: this.$route.params.productId,
+      currentProduct: []
+    }
+  },
+  mounted: function () {
+    this.getData()
+  },
+  methods: {
+    getData: function () {
+      this.$http({
+        url: 'https://erply-challenge.herokuapp.com/list?AUTH=fae7b9f6-6363-45a1-a9c9-3def2dae206d',
+        method: 'GET'
+      }).then(function (response) {
+        // success callback
+        var data = response.body
+        this.productsList = data
+        this.currentProduct = []
+        for (var key in this.productsList) {
+          if (key === this.$route.params.productId) {
+            this.currentProduct.push(this.productsList[key - 1])
+          }
+        }
+      }, function (response) {
+        // error callback
+      })
     }
   }
 }
