8e4365f663a2d38ebc3c01cb111624dd9be40d88
Gregor Laan <gregor@github.ee>
2018-03-26 13:48:20 +0300

    Get product Id's from localStorage and display them

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,9 +1,41 @@
 <template>
-  <div></div>
+  <div>
+    <b-container class="cart">
+      <b-row>
+        <b-col>
+
+          <b-list-group>
+            <b-list-group-item v-bind:key="product" v-for="product in localCart">
+              {{ product }}
+            </b-list-group-item>
+          </b-list-group>
+
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
+  data () {
+    return {
+      localCart: []
+    }
+  },
+  mounted: function () {
+    this.getFromCart()
+  },
+  methods: {
+    getFromCart: function () {
+      // get strings from localStorage
+      var strings = Vue.localStorage.get('cart')
+      // strings from localStorage to array
+      this.localCart = strings.split(',')
+    }
+  }
 }
 </script>
 
